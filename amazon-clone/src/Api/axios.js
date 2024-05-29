2e7f548134ff34c98776546cd46e9cb83a245b52
sammy-sasa <151463535+sammysamuti@users.noreply.github.com>
2024-05-29 20:00:41 +0300

    update

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5000",
-  baseURL: "https://amazon-api-by-samiya.cyclic.app",
+  baseURL: "https://amazon-backend-l42o.onrender.com",
 });
 
 export { axiosInstance };
